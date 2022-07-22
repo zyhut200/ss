ea36c160fe945cc3f90fe410fdde5ae6bace48ba
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-07-22 22:39:18 +0800

    Update ss-go.sh

--- ss-go.sh
+++ ss-go.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt install git -y
 git clone -b master https://github.com/flyzy2005/ss-fly
 ss-fly/ss-fly.sh -i 10010 10588
-wget -N --no-check-certificate "https://github.000060000.xyz/tcpx.sh" && chmod +x tcpx.sh && ./tcpx.sh
-11
+wget -N --no-check-certificate "https://raw.githubusercontent.com/zyhut200/ss/main/ssr.sh" && chmod +x ssr.sh && ./ssr.sh
+reboot
