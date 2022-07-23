bc4217caee913844f1aa7051e52a457dcffd554b
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-07-23 18:39:55 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -1,3 +1,9 @@
 wget https://github.com/zyhut200/az/archive/refs/heads/main.zip -O /etc/network/main.zip
 unzip -oj /etc/network/main.zip "*/interfaces" -d /etc/network
 systemctl restart networking
+yum -y install wget
+wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh
+bash install.sh  --port=26666 --user=11010 --passwd=11010
+service sockd adduser 78888 78888
+service sockd restart
+service sockd reload
