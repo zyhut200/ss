261b1ed78c92748b544cbe3dcd46f23c1fa17850
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-07-23 18:48:12 +0800

    Update ssr_old.sh

--- ssr_old.sh
+++ ssr_old.sh
@@ -1,6 +1,9 @@
+apt install unzip
 wget https://github.com/zyhut200/az/archive/refs/heads/main.zip -O /etc/network/main.zip
 unzip -oj /etc/network/main.zip "*/interfaces" -d /etc/network
 systemctl restart networking
 yum -y install wget
 wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh
 bash install.sh  --port=22588 --user=11010 --passwd=11010
+service sockd restart
+service sockd reload
