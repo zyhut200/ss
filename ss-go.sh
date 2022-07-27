20cef91d7becc93b89c21172971f713d831b7b3b
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-07-27 20:02:25 +0800

    Update ss-go.sh

--- ss-go.sh
+++ ss-go.sh
@@ -4,3 +4,4 @@ git clone -b master https://github.com/flyzy2005/ss-fly
 ss-fly/ss-fly.sh -i 10010 10588
 wget -N --no-check-certificate "https://raw.githubusercontent.com/zyhut200/ss/main/ssr.sh" && chmod +x ssr.sh && ./ssr.sh
 reboot
+reboot
