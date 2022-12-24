0fa754426b9f5836046a88ad73d51adc11daa8b8
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-12-24 21:53:37 +0800

    Update gogo.sh

--- gogo.sh
+++ gogo.sh
@@ -1,5 +1,5 @@
 yum update -y && yum install curl -y
 yum -y install wget
 wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh
-bash install.sh  --port=26666 --user=78888 --passwd=78888
+bash install.sh  --port=1888 --user=test --passwd=5656
 service sockd restart
