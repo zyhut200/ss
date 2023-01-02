1ee38b2c4ffe9fa896138c0885f24cde84cae19c
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-01-02 11:26:03 +0800

    Update ss-go.sh

--- ss-go.sh
+++ ss-go.sh
@@ -1,4 +1,5 @@
-apt-get update -y && apt-get install curl -y
+sudo apt update -y
+sudo apt install git -y
 git clone -b master https://github.com/flyzy2005/ss-fly
 ss-fly/ss-fly.sh -i 10010 10588
 wget -N --no-check-certificate "https://raw.githubusercontent.com/zyhut200/ss/main/ssr.sh" && chmod +x ssr.sh && ./ssr.sh
