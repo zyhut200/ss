5755b73be8b41ecec26c8b448bc6bc29691ab9ad
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-02-12 20:16:57 +0800

    Update sk5amz.sh

--- sk5amz.sh
+++ sk5amz.sh
@@ -4,3 +4,4 @@ wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh
 bash install.sh  --port=11688 --user=10010 --passwd=10010
 service sockd restart
 wget --no-check-certificate https://raw.githubusercontent.com/zyhut200/ss/main/deny.sh -O deny.sh
+bash deny.sh
